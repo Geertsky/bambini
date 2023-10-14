a1b4ee7ea0e8d52629ad529a66dbd84f58b7a501
Geertsky <122536131+Geertsky@users.noreply.github.com>
2023-10-14 23:53:16 +0200

    Deleted obsolete file

--- files/genfstab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'LABEL=root	/	xfs	defaults,x-systemd.device-timeout=0 0 0' >/mnt/etc/fstab
-echo 'LABEL=boot	/boot	xfs	defaults	0 0'>>/mnt/etc/fstab
-echo '/dev/vda2		/boot/efi	vfat	umask=0077,shortname=winnt 0 2' >>/mnt/etc/fstab
